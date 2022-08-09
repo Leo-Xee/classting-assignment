148b89b27a0e02110cdbd4f13f3de40d47b0a24b
Leo-Xee <jang.xmin@gmail.com>
2022-08-09 16:43:41 +0900

    chore: emotion 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Global } from "@emotion/react";
 import App from "./App";
+
+import GlobalStyle from "./styles/GlobalStyle";
 
 const element = document.getElementById("root");
 if (!element) throw Error("Root container missing in index.html");
 
 const root = ReactDOM.createRoot(element);
-root.render(<App />);
+root.render(
+  <>
+    <Global styles={GlobalStyle} />
+    <App />
+  </>,
+);
