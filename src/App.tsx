c9dba0c3b8947b2e65f72a74ab48c0d01d9c1e23
Leo-Xee <jang.xmin@gmail.com>
2022-08-10 18:28:58 +0900

    feat: 기본 레이아웃 설정 및 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import styled from "@emotion/styled";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "@/pages/Home";
+import Quiz from "@/pages/Quiz";
+
+const Layout = styled.section`
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  padding: 10px;
+`;
 
 function App() {
-  return <h1>Welcome to the React-Starter-Pack</h1>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
