7ba575aeea7362f17d16013fcd6fd09cdbc6ea90
Leo-Xee <jang.xmin@gmail.com>
2022-08-10 23:04:41 +0900

    feat: Selector 컴포넌트 구현

---
+++ src/components/common/Selector/Selector.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+type Props = {
+  label: string;
+  options: string[];
+  value: string | number;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  placeholder: string;
+};
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-size: 2rem;
+  font-weight: 900;
+
+  & select {
+    width: 100%;
+    padding: 10px;
+    appearance: none;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+`;
+
+function Selector({ label, options, value, onChange, placeholder }: Props) {
+  return (
+    <Label htmlFor={label}>
+      {label}
+      <select id={label} value={value} onChange={onChange}>
+        <option value="" disabled>
+          {placeholder}
+        </option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </Label>
+  );
+}
+
+export default Selector;
