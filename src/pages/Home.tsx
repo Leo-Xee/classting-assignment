c3a201543aba0f473a11b6c4e24b9f76114bba63
Leo-Xee <jang.xmin@gmail.com>
2022-08-10 23:50:48 +0900

    feat: Home 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+import Button from "@/components/common/Button";
+import Title from "@/components/Title";
+import QuizOption from "@/components/QuizOption";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  & h1 {
+    flex: 7;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const Control = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    navigate("/quiz");
+  };
+
+  return (
+    <Container>
+      <Title />
+      <Control>
+        <QuizOption />
+        <Button onClick={onClickHandler}>시작하기</Button>
+      </Control>
+    </Container>
+  );
 }
 
 export default Home;
