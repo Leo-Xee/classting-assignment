3f12dba64d7822fd2da2dc70842e03b103604b5c
Leo-Xee <jang.xmin@gmail.com>
2022-08-11 21:13:07 +0900

    feat: ProgressBar 컴포넌트 구현

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import styled from "@emotion/styled";
+import useQuizStore from "@/hooks/useQuizState";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  font-size: 2rem;
+  font-weight: 900;
+`;
+
+const ProgressContainer = styled.div`
+  width: 100%;
+  height: 10px;
+  background-color: #e5e5e5;
+  border-radius: 4px;
+`;
+
+const Progress = styled.div<{ percent: number }>`
+  width: ${({ percent }) => `${percent}%`};
+  height: 10px;
+  background-color: #00c896;
+  border-radius: 4px;
+  transition: 1s ease;
+`;
+
+function ProgressBar() {
+  const { page, count } = useQuizStore();
+
+  const percent = (page / count) * 100;
+
+  return (
+    <Container>
+      {page} / {count}
+      <ProgressContainer>
+        <Progress percent={percent} />
+      </ProgressContainer>
+    </Container>
+  );
+}
+
+export default ProgressBar;
