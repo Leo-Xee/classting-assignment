0d5bc9e1bd80ea2eea5840429353d0ce9480fc16
Leo-Xee <jang.xmin@gmail.com>
2022-08-13 18:14:03 +0900

    refactor: Button 컴포넌트 타입 수정

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,25 +1,25 @@
 import styled from "@emotion/styled";
 import { darken } from "polished";
-import React from "react";
+import React, { CSSProperties } from "react";
 
-type Props = {
+type Props = CSSProperties & {
   children: React.ReactNode;
   onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void;
-  bgColor?: string;
   disabled?: boolean;
 };
 
-const ButtonBase = styled.button<Pick<Props, "bgColor">>`
-  width: 100%;
+const ButtonBase = styled.button<Props>`
+  width: ${({ width }) => width};
   height: 65px;
-  background-color: ${({ bgColor }) => bgColor};
+  background-color: ${({ backgroundColor }) => backgroundColor};
   border-radius: 10px;
   font-size: 2.4rem;
   font-weight: 900;
-  color: #ffffff;
+  color: ${({ color }) => color};
 
   &:active {
-    background-color: ${({ bgColor }) => darken(0.03, `${bgColor}`)};
+    background-color: ${({ backgroundColor }) =>
+      darken(0.03, `${backgroundColor}`)};
   }
 
   &:disabled {
@@ -27,13 +27,22 @@ const ButtonBase = styled.button<Pick<Props, "bgColor">>`
   }
 `;
 
-function Button({ children, onClick, disabled, bgColor = "#00C896" }: Props) {
+function Button({
+  children,
+  onClick,
+  disabled,
+  width = "100%",
+  color = "#fff",
+  backgroundColor = "#00C896",
+}: Props) {
   return (
     <ButtonBase
       type="button"
       onClick={onClick}
       disabled={disabled}
-      bgColor={bgColor}
+      width={width}
+      color={color}
+      backgroundColor={backgroundColor}
     >
       {children}
     </ButtonBase>
